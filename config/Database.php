632b548659c019c9d012c76032c209b126f0fb23
Matthew Festa <festa.matthew@me.com>
2022-11-14 21:39:43 -0500

    Added DB Class

---
+++ config/Database.php
@@ -0,0 +1,27 @@
+<?php
+    class Database {
+        // DB Parameters
+        private $host = 'localhost';
+        private $db_name = 'myblog';
+        private $username = 'root';
+        private $password = '123456';
+        private $conn;
+
+        // DB Connection
+        public function connect() {
+            $this -> conn = null; 
+
+            // Create new PDO using try / catch
+            try {
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, 
+                $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                echo 'Connection Error ' .  $e->getMessage();
+            }
+
+            return $this->conn;
+        }
+    }
+
+?>
