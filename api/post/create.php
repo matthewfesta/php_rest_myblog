54d3e0d3fcf0beceea3a8cd8243ee7bac6fed7ee
Matthew Festa <festa.matthew@me.com>
2022-11-15 21:36:28 -0500

    added

--- api/post/create.php
+++ api/post/create.php
@@ -4,7 +4,12 @@
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
-    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+    header('Access-Control-Allow-Headers: 
+            Access-Control-Allow-Headers, 
+            Content-Type, 
+            Access-Control-Allow-Methods, 
+            Authorization, 
+            X-Requested-With');
 
     include_once '../../config/Database.php';
     include_once '../../models/Post.php';
